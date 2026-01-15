acadb020d2b11aae513e8543796625527824ae4f
metwas <metwas.seup@gmail.com>
2026-01-15 09:38:59 +0000

    GPL-v2 License

--- src/modules/threads/ThreadPool.ts
+++ src/modules/threads/ThreadPool.ts
@@ -1,26 +1,19 @@
 /**
-     MIT License
-
-     @Copyright (c) Metwas
-
-     Permission is hereby granted, free of charge, to any person obtaining a copy
-     of this software and associated documentation files (the "Software"), to deal
-     in the Software without restriction, including without limitation the rights
-     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     copies of the Software, and to permit persons to whom the Software is
-     furnished to do so, subject to the following conditions:
-
-     The above Copyright notice and this permission notice shall be included in all
-     copies or substantial portions of the Software.
-
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     SOFTWARE.
-*/
+ * Copyright (c) Metwas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 /**_-_-_-_-_-_-_-_-_-_-_-_-_- Imports _-_-_-_-_-_-_-_-_-_-_-_-_-*/
 
@@ -50,12 +43,7 @@ export class ThreadPool {
               this._persistent = options.persistent ?? true;
               this._auto = options.size === "auto";
 
-              this._size = this._auto
-                     ? cpus().length / 2
-                     : typeof options.size === "number"
-                       ? options.size
-                       : cpus().length / 2;
-
+              this._size = this._csize(options.size);
               this._max = options.maxQueueSize;
               this._file = options.file;
 
@@ -467,5 +455,20 @@ export class ThreadPool {
               ) {
                      this._spawn(id);
               }
+       }
+
+       /**
+        * Calculate thread count based on the @see ThreadPoolOptions
+        *
+        * @private
+        * @param {Number} size
+        * @returns {Number}
+        */
+       private _csize(size?: number | string): number {
+              return this._auto
+                     ? cpus().length / 2
+                     : typeof size === "number"
+                       ? size
+                       : cpus().length / 2;
        }
 }
